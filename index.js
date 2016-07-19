4f2e95254c3a606606864fdf1a2578784ec4341c
Gene Lee <genejaelee@gmail.com>
2016-07-19 16:20:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import experimentManager from 'tiny-experiment';
+
+angular.module('angular.experiment', [])
+
+.service('$experiment', ['$rootScope', function($rootScope) {
+  
+  $rootScope._experimentManager = experimentManager;
+  let manager = $rootScope._experimentManager;
+
+  return manager;
+}])
+
+.directive('tinyExperiment', ['$experiment', '$parse', ($experiment, $parse) => {
+  return {
+    scope: {
+      defaultVariant: '='
+    },
+    restrict: 'A',
+    link: (scope, elem, attr) => {
+      scope.key = attr.key || attr.tinyExperiment;
+      scope.variant = attr.variant;
+
+      let experiment = $experiment.getExperiment(scope.key);
+      if (!experiment.tracked) experiment.run();
+      if (experiment && experiment.variantName != scope.variant) {
+        elem.hide();
+        elem.remove();
+      } else if (!experiment) {
+        if (!attr.defaultVariant) {
+          elem.hide();
+          elem.remove();
+        }
+      }
+    }
+  }
+}])
